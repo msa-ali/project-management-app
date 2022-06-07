0382083c0b5736d1fe770de52e4fa142f9e7a22f
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-06-07 22:40:48 +0530

    Project & Client Queries

---
+++ server/schema/schema.js
@@ -0,0 +1,69 @@
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
+
+// mongoose models
+const Project = require('../models/Project');
+const Client = require('../models/Client');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    })
+});
+
+// Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return Client.findById(parent.clientId)
+            },
+        },
+    }),
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+              return Project.find();
+            },
+          },
+          project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+              return Project.findById(args.id);
+            },
+          },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve() {
+                return Client.find();
+            }
+        },
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Client.findById(args.id);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+})
