305a3b82a6bc2ff0df67fbae4af5cbe644270e11
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-06-11 09:54:03 +0530

    client changes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('colors');
 const { graphqlHTTP } = require('express-graphql');
 const { connectDB } = require('./config/db');
@@ -10,6 +11,8 @@ const schema = require('./schema/schema');
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
